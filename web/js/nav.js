e1a0e10309ff67fe60b21be3f312654719001ed9
so-hard <qq1136339397@gmail.com>
2017-04-07 19:59:50 +0800

    重新再来- -.

--- web/js/nav.js
+++ web/js/nav.js
@@ -1,5 +1,6 @@
-// 手机端下拉菜单
+// - -.
 $(function(){
+  $(".title").addClass("the-animation");
   $(".icon").bind("click", function() {
     if ($(".nav-container").is(":visible")) {
       $(".nav-container").hide(800);
@@ -10,3 +11,55 @@ $(function(){
     }
   })
 })
+
+//动画 超级无敌长的js
+$(window).scroll(function () {
+  // 给大标题加动画
+  var clearNav = $(".nav-wz");
+  var theTitle = $(".wrap-out-wz h1");
+  var winTop = $(window).scrollTop();
+  console.log(winTop);
+
+  if (winTop > 70) {
+    clearNav.addClass("nav-color");
+
+  }
+  else {
+    clearNav.removeClass("nav-color");
+  }
+  if (winTop  > 200) {
+    theTitle.css({
+      "opacity" : "1"
+  });
+  }
+})
+
+
+// meanbody 的 js
+$(function () {
+    var html = $(".left li:eq(0)");
+    var css = $(".left li:eq(1)");
+    var js = $(".left li:eq(2)");
+    var htmlBox = $(".right:eq(0)");
+    var cssBox = $(".right:eq(1)");
+    var jsBox = $(".right:eq(2)");
+    cssBox.hide();
+    jsBox.hide();
+    css.click(function () {
+      htmlBox.hide();
+      jsBox.hide();
+      cssBox.fadeIn(800);
+    })
+    html.click(function () {
+      cssBox.hide();
+      jsBox.hide();
+      htmlBox.fadeIn(1200);
+    })
+    js.click(function () {
+      cssBox.hide();
+      htmlBox.hide();
+      jsBox.fadeIn(800);
+    })
+})
+
+var addAnimation = $()
